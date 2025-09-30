ab1c85b05b9cb2c97d17b79075053ec9b2eb8973
Shriya Shintre <shriyashintre.20@gmail.com>
2025-09-30 17:56:11 +0530

    Assignment 1 - Data Types

---
+++ src/language/basics/DataTypesExamples.java
@@ -0,0 +1,92 @@
+package language.basics;
+
+public class DataTypesExamples 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		//Temperature of a city in degrees Celsius: 25.5 
+		float temperature = 25.5f;
+		System.out.println("Temperature of a city in degrees Celsius is : " + temperature);
+       
+		//Whether a customer has placed an order: true or false 
+		boolean order = false;
+		System.out.println("Customer has placed an order is : " + order);
+		
+		//Person's phone number: "123-456-7890" 
+		String phoneNum = "123-456-7890";
+		System.out.println("Person's phone number is : " + phoneNum);
+		
+		// Amount of money in a customer's bank account: 1000.50
+		float moneyAmt = 1000.50f;
+		System.out.println("Amount of money in a customer's bank account : " + moneyAmt);
+		
+		//Person's email address: "john.doe@example.com
+		String email = "john.doe@example.com";
+		System.out.println("Person's email address : " + email);
+		
+		//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		float location = 37.7749f;
+		double location1 = -122.4194;
+		System.out.println("Coordinates of a location latitude : " + location);
+		System.out.println("Coordinates of a location longitude : " + location1);
+		
+		// Person's marital status: true or false 
+		boolean status = true;
+		System.out.println("Person's marital status : " + status);
+		
+		// Person's occupation: "Software Engineer"
+		String occupation  = "Software Engineer"; 
+		System.out.println("Person's occupation : " + occupation);
+		
+		//Person's favourite colour: "Blue" 
+		String colour = "blue";
+		System.out.println("Person's favourite colour : " + colour);
+		
+		//Current year: 2023
+		int year = 2023;
+		System.out.println("Current year : " + year);
+		
+		//Number of followers on a social media platform: 1,000,000
+		String followers = "1,000,000";
+		System.out.println("Number of followers on a social media platform : " + followers);
+		
+		//Rating of a movie: 7.5 
+		float rating = 7.5f;
+		System.out.println("Rating of a movie : " + rating);
+		
+		//Person's blood type: 'A' 
+		char type = 'A';
+		System.out.println("Person's blood type :" + type);
+		
+		//Title of a book: "To Kill a Mockingbird" 
+		String book = "To Kill a Mockingbird";
+		System.out.println("Title of a book : " + book);
+		
+		//Number of employees in a company: 500 
+		int employee = 500;
+		System.out.println("Number of employees in a company : " + book);
+		
+		//Time of an event: 2:30 PM 
+		String time = "2:30 PM";
+		System.out.println("Time of an event : " + time);
+		
+		//Name of a country: "United States" 
+		String country = "United States";
+		System.out.println("Name of a country : " + country);
+		
+		//Person's eye color: "Brown"
+		String color = "Brown";
+		System.out.println("Person's eye color :" + color);
+		
+		//Person's birthplace: "New York City"
+		String birthplace = "New York City";
+		System.out.println("Person's birthplace : " + birthplace);
+		
+		//Distance between two cities: 200.5 
+		float distance = 200.5f;
+		System.out.println("Distance between two cities : " + distance);
+		
+		}
+
+}
--- src/language/basics/HelloWorld.java
+++ src/language/basics/HelloWorld.java
@@ -2,9 +2,10 @@ package language.basics;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello world");
 	}
 
 }
